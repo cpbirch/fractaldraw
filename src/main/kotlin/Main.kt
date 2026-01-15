09b83e7c3dd2a952557110e0100ef1a561a3bbf8
Chris Birch <chris.birch@holidays.ba.com>
2026-01-15 12:21:13 +0000

    move zoom in fractalPlane creation to separate function
    Signed-off-by: Chris Birch <chris.birch@holidays.ba.com>

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -49,55 +49,29 @@ fun loop(graphics2D: Graphics2D, args: Arguments) {
     var fp = FractalPlane(left, right, top, graphics2D.width, graphics2D.height, graphics2D.aspectRatio)
     val calculator = Mandlebrot(fp.MAX_I)
     val maxY = graphics2D.height / parallel
-    // println("maxY: $maxY")
-    // println("minRowBytes: ${graphics2D.minRowBytes()}")
 
     val timeSource = TimeSource.Monotonic
     var mark1 = timeSource.markNow()
     var mark2 = mark1
 
-//    var paletteBytes = (0..graphics2D.width)
-//        .flatMap { fp.colourPaletteBytes(it) }
-//        .take(fp.pixelWidth * 4) // 4 bytes per pixel
-//        .toList()
     var y = 0
-    while (!graphics2D.isWindowClosing()) {
+    while (!graphics2D.isWindowClosing()) { // Render loop aka game loop
         if (y < maxY) {
-            // println("Calculating row $y of $maxY")
-            calculator.parallelEscapeColourBytesRows(fp, y, parallel).forEach { row ->
-                val yByte = row.row * graphics2D.minRowBytes().toInt()
-                var x = 0
-                row.escapeVals.forEach {
-                    graphics2D.pixels[yByte + x] = it
-                    x++
+            calculator.parallelEscapeColourBytesRows(fp, y, parallel)
+                .forEach { row ->
+                    val yByte = row.row * graphics2D.minRowBytes().toInt()
+                    var x = 0
+                    row.escapeVals.forEach {
+                        graphics2D.pixels[yByte + x] = it
+                        x++
+                    }
                 }
-            }
-//            if (y < 20) {
-//                val yByte = y * graphics2D.minRowBytes().toInt()
-//                var x = 0
-//                paletteBytes.forEach {
-//                    graphics2D.pixels[yByte + x] = it
-//                    x++
-//                }
-//            }
             y++
-        } else if (mark2 == mark1) {
+        } else if (mark2 == mark1) { // is y >= maxY and time not recorded - done rendering
             mark2 = timeSource.markNow()
             println("Time taken: ${mark2 - mark1}")
-        } else if (graphics2D.isZooming()) {
-            val zoomBox = graphics2D.getZoomBox()
-            val fTopLeft = fp.toFractalCoord(PixelCoord(zoomBox[0].toInt(), zoomBox[1].toInt()))
-            val fBottomRight = fp.toFractalCoord(PixelCoord(zoomBox[2].toInt(), zoomBox[3].toInt()))
-            fp = FractalPlane(
-                fLeft = fTopLeft.x(),
-                fRight = fBottomRight.x(),
-                fTop = fTopLeft.y(),
-                pixelWidth = graphics2D.width,
-                pixelHeight = graphics2D.height,
-                aspectRatio = graphics2D.aspectRatio,
-                MAX_I = fp.MAX_I
-            )
-            println("Zoom to box: $zoomBox to fractal coords: $fTopLeft to $fBottomRight")
+        } else if (graphics2D.isZooming()) { // have we just zoomed in?
+            fp = getZoomedInFractalPlane(graphics2D, fp) // get new fractal plane with zoomed in bounds
             y = 0
             mark1 = timeSource.markNow()
             mark2 = mark1
@@ -105,9 +79,24 @@ fun loop(graphics2D: Graphics2D, args: Arguments) {
         }
 
         graphics2D.writePixels()
-        graphics2D.pollEvents()
+        graphics2D.pollEvents() // must be on main thread and not in a callback
     }
-    // Render loop
+}
+
+fun getZoomedInFractalPlane(graphics2D: Graphics2D, fp: FractalPlane): FractalPlane {
+    val zoomBox = graphics2D.getZoomBox()
+    val fTopLeft = fp.toFractalCoord(PixelCoord(zoomBox[0].toInt(), zoomBox[1].toInt()))
+    val fBottomRight = fp.toFractalCoord(PixelCoord(zoomBox[2].toInt(), zoomBox[3].toInt()))
+    println("Zoom to box: $zoomBox to fractal coords: $fTopLeft to $fBottomRight")
+    return FractalPlane(
+        fLeft = fTopLeft.x(),
+        fRight = fBottomRight.x(),
+        fTop = fTopLeft.y(),
+        pixelWidth = graphics2D.width,
+        pixelHeight = graphics2D.height,
+        aspectRatio = graphics2D.aspectRatio,
+        MAX_I = fp.MAX_I
+    )
 }
 
 fun destroy(graphics2D: Graphics2D) {
